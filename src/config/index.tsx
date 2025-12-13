ddda33dd3d0f38fe691ff89dcc66751befcb762c
Guoqi Sun <hi@guoqi.dev>
2025-12-13 23:38:17 +0700

    feat: correct github address

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,7 +1,7 @@
 export const socialLinks = {
   telegram: 'https://t.me/guoqisun',
   x: 'https://x.com/shutiaoorg',
-  github: 'https://github.com/shutiaoorg',
+  github: 'https://github.com/shutiao-org/shutiao-org',
   podcast: 'https://shutiao.life',
   youtube: 'https://www.youtube.com/@sun0225SUN',
   wechat: 'https://mp.guoqi.dev',
